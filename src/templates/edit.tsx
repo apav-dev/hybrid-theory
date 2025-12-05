0c117bbac610b65d6b9f1eb9fef52906c5269788
apav-dev <apavlick@yext.com>
2025-12-05 09:41:45 -0500

    adding edit template

---
+++ src/templates/edit.tsx
@@ -0,0 +1,70 @@
+import "@yext/visual-editor/editor.css";
+import "../index.css";
+import {
+  applyTheme,
+  defaultThemeConfig,
+  directoryConfig,
+  Editor,
+  locatorConfig,
+  mainConfig,
+  usePlatformBridgeDocument,
+  usePlatformBridgeEntityFields,
+  VisualEditorProvider,
+} from "@yext/visual-editor";
+import {
+  GetPath,
+  TemplateProps,
+  TemplateConfig,
+  HeadConfig,
+  TemplateRenderProps,
+  GetHeadConfig,
+} from "@yext/pages";
+import { type Config } from "@measured/puck";
+import tailwindConfig from "../../tailwind.config";
+
+const componentRegistry: Record<string, Config<any>> = {
+  main: mainConfig,
+  directory: directoryConfig,
+  locator: locatorConfig,
+};
+
+// Editor is available at /edit
+export const getPath: GetPath<TemplateProps> = () => {
+  return "edit";
+};
+
+export const config: TemplateConfig = {
+  name: "edit",
+};
+
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
+  document,
+}): HeadConfig => {
+  return {
+    other: applyTheme(document, defaultThemeConfig),
+  };
+};
+
+// Render the editor
+const Edit: () => JSX.Element = () => {
+  const entityDocument = usePlatformBridgeDocument();
+  const entityFields = usePlatformBridgeEntityFields();
+
+  return (
+    <VisualEditorProvider
+      templateProps={{
+        document: entityDocument,
+      }}
+      entityFields={entityFields}
+      tailwindConfig={tailwindConfig}
+    >
+      <Editor
+        document={entityDocument}
+        componentRegistry={componentRegistry}
+        themeConfig={defaultThemeConfig}
+      />
+    </VisualEditorProvider>
+  );
+};
+
+export default Edit;
